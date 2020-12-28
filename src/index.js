48c10a59b99c861a221ece05dc2a63c5f0fc5dea
Vimbi <i.am.vimbi@gmail.com>
2020-12-28 15:53:21 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return Array.isArray(array) ? Math.min(0,...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return Array.isArray(array) ? Math.max(0,...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return Array.isArray(array) && array.length != 0 ? array.reduce((a,b) => a + b, 0) / array.length : 0;
 }
